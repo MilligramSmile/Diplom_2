c4eafd7d61a545181ee3c60eb0a76e2d5533a309
Vadim Kovalev <onemilligramsmile@gmail.com>
2024-10-07 18:34:18 +0300

    fixes for conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,20 +7,15 @@ from help import User
 
 
 @pytest.fixture
-def user_full_cycle():                   # фикстура для создания, авторизации и удаления пользователя
+def user_registration_and_login():
     created_user = requests.post(f'{Endpoints.USER_REGISTER_URL}', data=User.valid_user)
     login_user = requests.post(f'{Endpoints.USER_LOGIN_URL}', data=User.valid_user)
+    yield created_user, login_user
+
+
+@pytest.fixture
+def user_full_cycle(user_registration_and_login):
+    created_user, login_user = user_registration_and_login
     yield created_user, login_user
     requests.delete(f'{Endpoints.USER_DELETE_URL}', data=User.valid_user)
 
-
-@pytest.fixture
-def user_registration_and_login():      # фикстура для регистрации и авторизации пользователя
-    created_user = requests.post(f'{Endpoints.USER_REGISTER_URL}', data=User.valid_user)
-    login_user = requests.post(f'{Endpoints.USER_LOGIN_URL}', data=User.valid_user)
-    yield created_user, login_user
-
-
-@pytest.fixture
-def valid_ingredients():                # фикстура для получения списка ингредиентов
-    return help.GetIngredients()
